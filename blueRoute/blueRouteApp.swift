d0fc978281a911696d8dfe625eb2c990c845f902
Sandro Giannini <sgianninigarcia@gmail.com>
2022-11-10 14:28:23 -0500

    Initial Commit

---
+++ blueRoute/blueRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  blueRouteApp.swift
+//  blueRoute
+//
+//  Created by Sandro Giannini on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct blueRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
